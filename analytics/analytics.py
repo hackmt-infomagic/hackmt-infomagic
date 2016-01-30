c271a1111e9b5545c621a98cdc7aedfe69c351ed
Joshua L. Phillips <joshua.lee.phillips@gmail.com>
2016-01-30 14:13:10 -0600

    General transition probabilities added and calc_global_probabilities changed to calc_probabilities

--- analytics/analytics.py
+++ analytics/analytics.py
@@ -61,21 +61,78 @@ def user_total(user_data):
 
   return total
 
-def calc_global_probabilities(user_data):
-    '''Calculate global probabilities of events given a list of sessions - including gaps'''
-    subjects = user_data['subjects']
-    probabilities = numpy.zeros(len(user_data['subjects'])+1)
+def calc_probabilities(user_data):
+    '''Calculate probabilities of events given a list of sessions - including gaps'''
+    ## Setup timestamps as datetime-timedelta
     convert_start_delta(user_data)
+
+    subjects = user_data['subjects'] + ['Not Tracked']
+    n = len(subjects)
+    state_probabilities = numpy.zeros(n)
+    ## Using this to avoid divide-by-zero, for now...
+    transition_probabilities = numpy.zeros((n,n))+0.00000001
     lastend = user_data['sessions'][0]['start']
     total = 0.0
+    gaps = []
+
+
+    ## Calculate overall state probabilities and gap lengths
     for session in user_data['sessions']:
         session_time = session['end']
         gap_time = session['start'] - lastend
-        probabilities[user_data['subjects'].index(session['subject'])] += session_time.total_seconds()
-        probabilities[len(probabilities)-1] += gap_time.total_seconds()
+        state_probabilities[user_data['subjects'].index(session['subject'])] += session_time.total_seconds()
+        state_probabilities[n-1] += gap_time.total_seconds()
         total += session_time.total_seconds() + gap_time.total_seconds()
         lastend += session_time + gap_time
-    probabilities /= total
+        gaps.append(gap_time.total_seconds())
+    state_probabilities /= total
+    gaps = numpy.array(gaps[1:])
+
+    ## Calculate kernel density estimate of user gaps
+    kde = KernelDensity(kernel='gaussian',bandwidth=300).fit(numpy.array(gaps).reshape(-1,1))
+    x_plot = numpy.linspace(0,max(gaps),1000)[:,numpy.newaxis]
+    dens = numpy.exp(kde.score_samples(x_plot))
+    ## Could plot this with matplotlib
+    ## fig,ax = plot.subplots(1,1,sharex=True,sharey=True)
+    ## ax.plot(x_plot[:,0],dens,'-',label="gap density")
+    ## ax.plot(x_plot[dens.tolist().index(max(dens))],max(dens),'or')
+
+    ## Calculate bottom 50%
+    total = 0.0
+    for x in range(len(dens)):
+        total += dens[x]
+        if total/sum(dens) > 0.75:
+            break
+    cutoff = numpy.asscalar(x_plot[x])
+    ## ax.plot(x_plot[x],dens[x],'or')
+    ## plot.show()
+    
+    ## Set long gaps to zero, since they are not part of the bottom half (connected events)
+    gaps[gaps > cutoff] = 0.0
+    
+    ## Calculate transition probabilities using non-zero gaps
+    for x in range(len(sessions)-1):
+        if gaps[x] > 0.0:
+            transition_probabilities[subjects.index(sessions[x]['subject']),subjects.index(sessions[x+1]['subject'])] += 1.0
+        else:
+            transition_probabilities[subjects.index(sessions[x]['subject']),len(subjects)-1] += 1.0
+            transition_probabilities[len(subjects)-1,subjects.index(sessions[x+1]['subject'])] += 1.0
+
+    ## Normalization
+    print(numpy.around(transition_probabilities,decimals=1))
+    print(numpy.around(transition_probabilities.sum(axis=1),decimals=1))
+    print(numpy.around(transition_probabilities.sum(axis=0),decimals=1))
+    t_from = transition_probabilities / transition_probabilities.sum(axis=1)[:,numpy.newaxis]
+    t_to = transition_probabilities / transition_probabilities.sum(axis=0)
+    
+    ## Cutoff for relevance
+    t_from[t_from<0.00000001] = 0.0
+    t_to[t_to<0.00000001] = 0.0
+    
+    ## Convert timestamps back to standard storage
     to_start_end(user_data)
     to_raw_timestamps(user_data)
-    return(probabilities)
+    return({'P':state_probabilities,
+            'from':t_from,
+            'to':t_to})
+
