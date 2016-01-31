8623a1504b78e0fc4c155783c5eb2ab6acf06a5a
Austin Ban <banaustinban@gmail.com>
2016-01-30 22:15:39 -0600

    created header and footer html and scss

--- public/javascripts/components/AppHeader.react.js
+++ public/javascripts/components/AppHeader.react.js
@@ -7,9 +7,48 @@ var React = require('react');
 var AppHeader = React.createClass({
     render: function(){
         return (
-            <div>
-                Put header code here!
-            </div>
+            <div className="fixed">
+  				<nav className="nav-bar top-bar" data-topbar role="navigation">
+  					<div className="row">
+  					<div className="small-2 columns">
+  						<a href="#">
+  							<img src="../images/white-menu.png" width="35px" height="35px"></img>
+  						</a>
+  					</div>
+  						<div className="small-8">
+		    				<h1>
+		    					Overview
+		    				</h1>
+    					</div>
+					<div className="small-2 columns align-middle">
+						<a href="#">
+							<img src="../images/white-gear.png" width="35px" height="35px"></img>
+						</a>
+					</div>
+					</div>
+  				</nav>
+
+
+  				<div className="footer">
+  					<div className="small-12 start-button columns">
+						<a href="#"><span></span>
+							<h1>
+								Start
+							</h1>
+						</a>
+  					</div>
+  					<div className="small-12 stop-button columns">
+						<a href="#"><span></span>
+							<h1>
+								Stop
+							</h1>
+						</a>
+  					</div>
+  				</div>
+
+
+			</div>
+
         );
     }
 });
--- public/javascripts/components/Overview.react.js
+++ public/javascripts/components/Overview.react.js
@@ -2,11 +2,29 @@
  * Created by rayde on 1/30/2016.
  */
 var React = require('react');
+var AppHeader = require('./AppHeader.react');
 
 var Overview = React.createClass({
     render: function(){
         return(
-            <div>Overview</div>
+        	<AppHeader />
+            
+  				<div className="footer">
+  					<div className="small-12 start-button columns">
+						<a href="#"><span></span>
+							<h1>
+								Start
+							</h1>
+						</a>
+  					</div>
+  					<div className="small-12 stop-button columns">
+						<a href="#"><span></span>
+							<h1>
+								Stopping
+							</h1>
+						</a>
+  					</div>
+  				</div>
         );
     }
 });
