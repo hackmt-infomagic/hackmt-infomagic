99ee50a7f020f1f5bd4799619f5a2f60a8f0a9ba
Geoffrey Gross <geoffreyrgross@gmail.com>
2016-01-30 22:24:21 -0600

    Set Overview has index. And imported GraphComponent.

--- public/javascripts/components/Overview.react.js
+++ public/javascripts/components/Overview.react.js
@@ -3,28 +3,31 @@
  */
 var React = require('react');
 var AppHeader = require('./AppHeader.react');
+var GraphComponent = require('./GraphComponent.react');
 
 var Overview = React.createClass({
     render: function(){
         return(
-        	<AppHeader />
-            
-  				<div className="footer">
-  					<div className="small-12 start-button columns">
+			<div>
+				<AppHeader />
+				<GraphComponent />
+				<div className="footer">
+					<div className="small-12 start-button columns">
 						<a href="#"><span></span>
 							<h1>
 								Start
 							</h1>
 						</a>
-  					</div>
-  					<div className="small-12 stop-button columns">
+					</div>
+					<div className="small-12 stop-button columns">
 						<a href="#"><span></span>
 							<h1>
 								Stopping
 							</h1>
 						</a>
-  					</div>
-  				</div>
+					</div>
+				</div>
+			</div>
         );
     }
 });
